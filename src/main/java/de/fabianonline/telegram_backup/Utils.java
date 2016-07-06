0d431beba6ae49a0400dfa8327f30fe0767171bf
Fabian Schlenz <mail@fabianonline.de>
2016-07-06 08:30:23 +0200

    Moved FLOOD_WAIT exception handling to Utils class.

--- src/main/java/de/fabianonline/telegram_backup/Utils.java
+++ src/main/java/de/fabianonline/telegram_backup/Utils.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 package de.fabianonline.telegram_backup;
-
+import com.github.badoualy.telegram.tl.exception.RpcErrorException;
 import java.io.File;
 import java.util.List;
 import java.util.Vector;
@@ -31,5 +31,26 @@ public class Utils {
 			}
 		}
 		return accounts;
+	}
+	
+	static void obeyFloodWaitException(RpcErrorException e) throws RpcErrorException {
+		if (e==null || ! e.getTag().startsWith("420: FLOOD_WAIT_")) return;
+		
+		int delay = Integer.parseInt(e.getTag().substring(16));
+		int minutes = (delay/60)+1;
+		int wait = (minutes / 5) * 5 + 5;
+		System.out.println("");
+		System.out.println(
+			"Telegram complained about us (okay, me) making too many requests in too short time by\n" +
+			"sending us \"" + e.getTag() + "\" as an error. So we now have to wait a bit. Telegram\n" +
+			"asked us to wait for " + delay + " seconds, which is about " + minutes + " minutes.\n" +
+			"I'm adding a few minutes to let the API recover, so we are going to wait for " + wait + " mins.\n" +
+			"\n" +
+			"So I'm going to do just that for now. If you don't want to wait, you can quit by pressing\n" +
+			"Ctrl+C. You can restart me at any time and I will just continue to download your\n" +
+			"messages and media. But be advised that just restarting me is not going to change\n" +
+			"the fact that Telegram won't talk to me until then.");
+		try { Thread.sleep(wait * 60 * 1000); } catch(InterruptedException e2) {}
+		System.out.println("");
 	}
 }
